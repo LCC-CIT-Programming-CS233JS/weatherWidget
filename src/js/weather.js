1c88eeea07da7717506d32f1899dc3e81b1c2a5a
goodmari <goodm@lanecc.edu>
2019-11-09 10:59:58 -0800

    Updated instructions to include second api call for the timezone information

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,6 +1,7 @@
 import './general';
 
 //http://api.openweathermap.org/data/2.5/forecast?zip=97405&units=imperial&appid=c59493e7a8643f49446baf0d5ed9d646
+//https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyC7QBEyXbpXf53jPvM4lXfgXEHD5caa61A
 
 /* Create a class called Weather
 - Part 1 - Retrieve the weather information when the user clicks the buttobn
@@ -15,6 +16,8 @@ import './general';
         };
         this.url = "http://api.openweathermap.org/data/2.5/forecast?zip=";
         this.apikey = "&units=imperial&appid=c59493e7a8643f49446baf0d5ed9d646";
+        this.googleApiKey = "AIzaSyC1HTCZ6mUEKFuuLHPLdE1zM2_Q7j0vxhk";
+        this.googleMapsUrl = "https://maps.googleapis.com/maps/api/timezone/json?location=";
     - initialize instance variables for UI elements
         the form
         the zipcode input element
@@ -23,28 +26,52 @@ import './general';
     - write the stub of a method onFormSubmit
     - bind the class to onFormSubmit
     - add a submit handler to the form that calls onFormSubmit
-  - Write the method onFormSubmit.  It should
+  - Write the first version of the method onFormSubmit.  It should
     - prevent the form from being sumbitted to the server
     - get the zip code from the UI and put it in a variable
-    - call fetch with the url zipcode and apikey
+    - call fetch with the url zipcode and apikey for openweathermap
       - when the response comes back THEN parse the json
       - when that finishes THEN 
         - set the city in the state object
         - set the forecast in the state object
-        - set the simpleForecast in the state object 
-            by calling the method parseForecast (see bottom of file)
         - set the selectedDate to null
         - clear the zipcode from the UI
-        - call the method renderWeatherList and pass this.state.simpleForecast as the arg
-  - Write a first version of renderWeatherList.  It has forecast (which is 5 element simplified forcast array) as a parameter.
-    - console.log the value of forecast.
-  - Edit the constructor to bind the class to the method renderWeatherList
+        - console.log the forecast and make sure that your ajax call works
+  - Don't forget to instantiate the a weather object!
 END OF PART 1 - TEST AND DEBUG YOUR APP
-- Part 2 - Format ONE weather list item and the weather list as a whole
-  - Write the method renderWeatherListItem
-    - This method returns a template literal containing the html for the weather for ONE day.
-      It gets called in renderWeatherList.  It has 2 parameters a forecastDay and an index.
-      The forecastDay is a js object from the "parsed" version of the return from the weather api.
+- Part 2 - Add a second ajax call to google maps to get timezone information
+  for the zipcode.  The dates in the weather information are GMT.  You want
+  to translate them to the timezone for the zipcode the user entered.
+  - Edit the method onFormSubmit
+      - replace the console.log of the forecast (the original weather data)
+        with an additional call to fetch
+        - call fetch with the url for google maps 
+        ** fetch(`${this.googleMapsUrl}
+                ${this.state.city.coord.lat},${this.state.city.coord.lon}
+                &timestamp=${this.state.forecast[0].dt}
+                &key=${this.googleApiKey}`) **
+          - when the response comes back THEN parse the json
+          - when that finishes THEN
+            - calculate the timezone offset using the rawOffset and dstOffset from google
+              this.state.timezoneOffset =  (data.rawOffset + data.dstOffset) / (60 * 60);
+            - set the simpleForecast in the state object by calling parseForecast (bottom of the file)  
+              this.state.simpleForecast = this.parseForecast(this.state.forecast, this.state.timezoneOffset);
+            - console.log the simpleForecast 
+END OF PART 2 - TEST AND DEBUG YOUR APP
+- Part 3 - Write the first version of method renderWeatherList.  It writes the simpleForecast data to the page
+  - Write a stub of renderWeatherListItem.  This method returns a template literal containing the html 
+    for the weather for ONE day.  It gets called in renderWeatherList.  It has 2 parameters a 
+    forecastDay and an index.  The forecastDay is a js object from the "parsed" version of the return from the weather api.
+    - in the body of the method console.log both the forecastDay and the index
+  - Write a sub of renderWeatherList.  It has forecastDays (which is 5 element simplified forcast array) 
+    as a parameter.
+    - in the body of the method console.log the value of forecastDays.
+  - Edit the constructor to bind the class to the method renderWeatherList
+  - call renderWeatherList in onFormSubmit after both ajax calls have completed and the forecast data
+    has been parsed.  Pass this.state.simpleForecast as a parameter.
+END OF PART 3 - TEST AND DEBUG YOUR APP
+- Part 4 - Format ONE weather list item and the weather list as a whole
+  - Edit the body of the method renderWeatherListItem
     - Format the weather information for one day on the html page.  At a minimum it should include
       - the month and day as well as the weekday
       - the high and low temperatures for that day
@@ -56,12 +83,12 @@ END OF PART 1 - TEST AND DEBUG YOUR APP
       - Return the template literal 
   - Edit the body of the method renderWeather list.  It should
     - Create the html for each of the weather list items.  Use the array method map to do this.
-      const itemsHTML = forecast.map((forecastDay, index) => this.renderWeatherListItem(forecastDay, index)).join('');
+      const itemsHTML = forecastDays.map((forecastDay, index) => this.renderWeatherListItem(forecastDay, index)).join('');
     - Set the inner html of the weatherList element on the page to 
       - a div element styled with weather-list flex-parent
       - that contains the itemsHTML from above
-END OF PART 2 - TEST AND DEBUG YOUR APP
-- Part 3 - Display weather details when the user clicks one weather list item
+END OF PART 4 - TEST AND DEBUG YOUR APP
+- Part 5 - Display weather details when the user clicks one weather list item
   - Write the method renderCurrentDay.  It takes the index of the day as it's parameter.
     - Format the detailed weather information for the selected day on the html page. Include at least
       - identifying information for the city as well as the date
@@ -75,16 +102,45 @@ END OF PART 2 - TEST AND DEBUG YOUR APP
   - Add a click event handler to each of the weather list items 
     - add a loop to the end of the renderWeatherList method that adds the event handler
     - you'll have to bind the method renderCurrentDay to both the class and the index of the item
+      forecastElements[i].onclick = this.renderCurrentDay.bind(this, i)
   - Write the method clearCurrentDay.  It sets the inner html property of the currentDay element to ""
-  - Call clearCurrentDay at the end of onFormSubmit
-END OF PART 3 - TEST AND DEBUG YOUR APP
+  - Call clearCurrentDay at the end of onFormSubmit where you clear the zipcode.  
+END OF PART 5 - TEST AND DEBUG YOUR APP
 */
 
-// Don't forget to instantiate the a weather object!
+
 
 /*
-  parseForecast(forecast) {
+  getIndexOfMidnight(firstDate, timezoneOffset) {
+    let dt = firstDate * 1000;
+    let date = new Date(dt);
+    let utcHours = date.getUTCHours();
+    let localHours = utcHours + timezoneOffset;
+    let firstMidnightIndex = (localHours > 2 ) ? 
+        Math.round((24 - localHours)/3) : 
+        Math.abs(Math.round(localHours / 3));
+    return firstMidnightIndex;
+  }
+
+  findMinTemp(forecast, indexOfMidnight) {
+    let min = forecast[indexOfMidnight].main.temp_min;
+    for (let i = indexOfMidnight + 1; i < indexOfMidnight + 8; i++)
+      if (forecast[i].main.temp_min < min)
+        min = forecast[i].main.temp_min;
+    return min;
+  }
+
+  findMaxTemp(forecast, indexOfMidnight) {
+    let max = forecast[indexOfMidnight].main.temp_max;
+    for (let i = indexOfMidnight + 1; i < indexOfMidnight + 8; i++)
+      if (forecast[i].main.temp_max > max)
+        max = forecast[i].main.temp_max;
+    return max;
+  }
+
+  parseForecast(forecast, timezoneOffset) {
     let simpleForecast = new Array();
+    const MIDNIGHT = this.getIndexOfMidnight(forecast[0].dt, timezoneOffset);
     const NOON = 4;
     const SIXAM = 2;
     const SIXPM = 6;
@@ -94,18 +150,18 @@ END OF PART 3 - TEST AND DEBUG YOUR APP
     const EVENING = SIXPM;
     const NIGHT = NINEPM;
     const PERDAY = 8;
-    const DAYS = 5;
-    for (let i = 0; i < forecast.length; i+=PERDAY) {
+    const DAYS = 4;
+    for (let i = MIDNIGHT; i < forecast.length - NINEPM; i+=PERDAY) {
       let oneDay = new Object();
       oneDay.dt = forecast[i + NOON].dt;
       oneDay.temp = forecast[i + NOON].main.temp;
-      oneDay.minTemp = forecast[i + SIXAM].main.temp_min;
-      oneDay.maxTemp = forecast[i + SIXPM].main.temp_max;
-      oneDay.morningTemp = 
-      oneDay.dayTemp = 
-      oneDay.eveningTemp = 
-      oneDay.nightTemp = 
-      oneDay.description = 
+      oneDay.minTemp = this.findMinTemp(forecast, i);
+      oneDay.maxTemp = this.findMaxTemp(forecast, i);
+      oneDay.morningTemp = forecast[i + MORNING].main.temp;
+      oneDay.dayTemp = forecast[i + DAY].main.temp;
+      oneDay.eveningTemp = forecast[i + EVENING].main.temp;
+      oneDay.nightTemp = forecast[i + NIGHT].main.temp;
+      oneDay.description = forecast[i + NOON].weather[0].description;
       oneDay.icon = 
       oneDay.pressure = 
       oneDay.wind = 
